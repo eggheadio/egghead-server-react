4eb98e0e743c2b76a6f3175f563cdfe6421f8695
ijones16 <jones58ian@gmail.com>
2015-09-17 10:07:04 -0700

    updated to react 0.14.0-rc1

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var React = require('react');
+var ReactDOMServer = require('react-dom/server');
 var Component = React.createFactory(require('./Component'));
 
 
@@ -8,7 +9,10 @@ var app = express();
 function home(req, res){
   var msg = req.params.msg || 'Hello';
   var comp = Component({msg:msg});
-  res.send(React.renderToStaticMarkup(comp));
+
+// React.renderToStaticMarkup is deprecated. 
+// Please use ReactDOMServer.renderToStaticMarkup from require('react-dom/server') instead.
+  res.send(ReactDOMServer.renderToStaticMarkup(comp));
 }
 
 app.get('', home);
