ce1d883bc1202c8abacaa2954905d9f34bee4d9f
Creeland <cree@provinsal.com>
2016-06-01 15:10:48 -0700

    Updated to ReactDOMServer

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var React = require('react');
+var ReactDOMServer = require('react-dom/server');
 var Component = React.createFactory(require('./Component'));
 
 
@@ -8,7 +9,10 @@ var app = express();
 function home(req, res){
   var msg = req.params.msg || 'Hello';
   var comp = Component({msg:msg});
-  res.send(React.renderToStaticMarkup(comp));
+
+  /* React.renderToStaticMarkup has moved to ReactDOMServer.renderToStaticMarkup
+  See http://facebook.github.io/react/docs/top-level-api.html#reactdomserver.rendertostaticmarkup */
+  res.send(ReactDOMServer.renderToStaticMarkup(comp));
 }
 
 app.get('', home);
